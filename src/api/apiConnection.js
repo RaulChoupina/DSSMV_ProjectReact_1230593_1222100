07bd83638ff12326160ad03604d8b81e0ce4e112
RaulChoupina <160479758+RaulChoupina@users.noreply.github.com>
2025-12-09 17:38:49 +0000

    files

---
+++ src/api/apiConnection.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://193.136.62.24/v1',
+  timeout: 5000,
+});
+
+export default api;
---
+++ src/context/AppContext.js
@@ -0,0 +1,7 @@
+// src/context/AppContext.js
+import React from 'react';
+
+const AppContext = React.createContext();
+
+export const { Provider, Consumer } = AppContext;
+export default AppContext;
